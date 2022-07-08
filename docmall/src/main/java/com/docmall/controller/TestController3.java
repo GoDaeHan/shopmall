57a5940882b168be8ae742421d84f516bd875fb5
audrnd77 <audrnd77@Gmail.com>
2022-07-08 17:02:18 +0900

    test

--- docmall/src/main/java/com/docmall/controller/TestController3.java
+++ docmall/src/main/java/com/docmall/controller/TestController3.java
@@ -1,5 +1,8 @@
 package com.docmall.controller;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TestController3 {
 
 }
--- docmall/src/main/java/com/docmall/controller/TestController6.java
+++ docmall/src/main/java/com/docmall/controller/TestController6.java
@@ -3,6 +3,6 @@ package com.docmall.controller;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TestController4 {
+public class TestController6 {
 
 }
